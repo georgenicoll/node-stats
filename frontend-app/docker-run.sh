c12fd04b540c33c803fc80d95bf8015bb34a603d
George Nicoll <george.nicoll@gmail.com>
2021-11-21 15:16:36 +0000

    Tweak docker build

--- frontend-app/docker-run.sh
+++ frontend-app/docker-run.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 docker run \
-  --publish 3000:3000 \
+  --rm \
+  --publish 3000:3456 \
+  --env PORT=3456 \
   --env BACKEND_DNS_ADDRESS=mandrill.lan \
   --env BACKEND_PORT=9000 \
-  --workdir /stats-frontend \
-  georgenicoll/stats-frontend \
-  node app/index.js
+  georgenicoll/stats-frontend
